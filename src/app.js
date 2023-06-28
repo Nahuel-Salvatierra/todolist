b6341ef99e042498f08601fcf5847e47dad840a7
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-06-28 02:13:05 -0300

    feat: :sparkles: connection to extern database server
    Se realizo la coneccion exitosa e implementacion de a una base de datos externa para almacenar los datos de la aplicacion.
    
    new dependence 'mysql2'

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config'
+import './database/servercc.js'
 
 const PORT = process.env.PORT
 const app = express();
--- src/database/servercc.js
+++ src/database/servercc.js
@@ -1,11 +1,24 @@
-import sql from 'mysql';
+import mysql from 'mysql2/promise';
+import 'dotenv/config'
 
-const dbSettings = {
-    user: '',
-    password: '',
-    server: '',
-    database: ''
+
+async function main () {
+    const connection = await mysql.createPool({
+        database: process.env.DATABASE_MYSQL,
+        user: process.env.USER_DB,
+        host: process.env.HOST_MYSQL,
+        password: process.env.USER_DB_PASSWORD,
+        ssl : {
+            rejectUnauthorized: false
+        }
+    
+    });
+    
+    const [rows] = await connection.query('select * from usuarios')
+    console.log(rows)
 }
 
+main()
 
-sql.connect(dbSettings)
+// getConnection();
+
